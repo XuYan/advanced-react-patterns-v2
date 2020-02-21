6f8fcaff977d3236310ae6dc7f6ed5e50e54d14c
XuYan <xuwithwill@gmail.com>
2020-02-20 20:29:25 -0800

    complete exercise 10 and add what I learnt from the exercise

--- src/__tests__/10.js
+++ src/__tests__/10.js
@@ -4,9 +4,9 @@ import {
   isCompositeComponentWithType,
 } from 'react-dom/test-utils'
 import chalk from 'chalk'
-import {fireEvent, renderToggle} from '../../test/utils'
-import Usage, {Toggle} from '../exercises-final/10'
-// import Usage, {Toggle} from '../exercises/10'
+import { fireEvent, renderToggle } from '../../test/utils'
+// import Usage, {Toggle} from '../exercises-final/10'
+import Usage, { Toggle } from '../exercises/10'
 
 const findToggleInstances = rootInstance =>
   findAllInRenderedTree(rootInstance, c =>
@@ -17,8 +17,8 @@ function validateToggleInstance(instance) {
   // validate the internal state of the toggle does not change
   // If it does change then you could trigger an unnecessary re-render
   try {
-    expect(instance.state).toEqual({on: false})
-    expect(instance.state).toEqual({on: false})
+    expect(instance.state).toEqual({ on: false })
+    expect(instance.state).toEqual({ on: false })
   } catch (error) {
     const helpfulMessage = chalk.red(
       `🚨  The toggle should not update its own state when it's controlled  🚨`,
@@ -30,7 +30,7 @@ function validateToggleInstance(instance) {
 
 test('toggling either toggle toggles both', () => {
   const handleToggle = jest.fn()
-  const {getAllByTestId, rootInstance} = renderToggle(
+  const { getAllByTestId, rootInstance } = renderToggle(
     <Usage onToggle={handleToggle} />,
   )
   const [toggleInstance1, toggleInstance2] = findToggleInstances(
@@ -52,7 +52,7 @@ test('toggling either toggle toggles both', () => {
 
 test('toggle can still be uncontrolled', () => {
   const handleToggle = jest.fn()
-  const {toggleButton, toggle} = renderToggle(
+  const { toggleButton, toggle } = renderToggle(
     <Toggle onToggle={handleToggle} />,
   )
   expect(toggleButton).toBeOff()
@@ -75,4 +75,8 @@ test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
   expect(submitted).toBe(true)
 })
+/**
+ * What is Control Props?
+ * Get states from props and provide a mechanism to suggest changes to those states.
+ */
 ////////////////////////////////
--- src/exercises/10.js
+++ src/exercises/10.js
@@ -1,14 +1,14 @@
 // control props
 
 import React from 'react'
-import {Switch} from '../switch'
+import { Switch } from '../switch'
 
 // Here we're going to simplify our component slightly so you
 // can learn the control props pattern in isolation from everything else.
 // Next you'll put the pieces together.
 
 class Toggle extends React.Component {
-  state = {on: false}
+  state = { on: false }
   // 🐨 let's add a function that can determine whether
   // the on prop is controlled. Call it `isControlled`.
   // It can accept a string called `prop` and should return
@@ -19,22 +19,23 @@ class Toggle extends React.Component {
   // whether it's coming from this.state or this.props
   // Call it `getState` and have it return on from
   // state if it's not controlled or props if it is.
+  isControlled = prop => this.props[prop] !== undefined
+  getState = prop => this.isControlled(prop) ? this.props[prop] : this.state[prop]
+
   toggle = () => {
     // 🐨 if the toggle is controlled, then we shouldn't
     // be updating state. Instead we should just call
     // `this.props.onToggle` with what the state should be
-    this.setState(
-      ({on}) => ({on: !on}),
-      () => {
-        this.props.onToggle(this.state.on)
-      },
-    )
+    const on = this.getState('on')
+    if (!this.isControlled('on')) {
+      this.setState({ on: !on })
+    }
+    this.props.onToggle(!on)
   }
   render() {
     // 🐨 rather than getting state from this.state,
     // let's use our `getState` method.
-    const {on} = this.state
-    return <Switch on={on} onClick={this.toggle} />
+    return <Switch on={this.getState('on')} onClick={this.toggle} />
   }
 }
 
@@ -51,13 +52,13 @@ class Toggle extends React.Component {
 // component is intended to be used and is used in the tests.
 // You can make all the tests pass by updating the Toggle component.
 class Usage extends React.Component {
-  state = {bothOn: false}
+  state = { bothOn: false }
   handleToggle = on => {
-    this.setState({bothOn: on})
+    this.setState({ bothOn: on })
   }
   render() {
-    const {bothOn} = this.state
-    const {toggle1Ref, toggle2Ref} = this.props
+    const { bothOn } = this.state
+    const { toggle1Ref, toggle2Ref } = this.props
     return (
       <div>
         <Toggle
@@ -76,4 +77,4 @@ class Usage extends React.Component {
 }
 Usage.title = 'Control Props'
 
-export {Toggle, Usage as default}
+export { Toggle, Usage as default }
